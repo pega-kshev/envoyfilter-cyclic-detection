c322dbe3bdc7133240340d7be8f008c55b8a8573
kshev <VinayKumar.KsheeraSagar@in.pega.com>
2024-09-24 18:42:10 +0530

    Improve Service calls

--- ServiceB/app.py
+++ ServiceB/app.py
@@ -7,13 +7,35 @@ app = Flask(__name__)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+# Define the maximum number of hops
+MAX_HOPS = 50
+
 @app.route('/')
 def call_serviceA():
     trace_id = request.headers.get('x-b3-traceid', 'default-trace-id')
-    headers = {'x-b3-traceid': trace_id}
+    span_id = request.headers.get('x-b3-spanid', 'default-span-id')
+    parent_span_id = request.headers.get('x-b3-parentspanid', 'default-parent-span-id')
+    sampled = request.headers.get('x-b3-sampled', '1')
+    flags = request.headers.get('x-b3-flags', '0')
+    hopcount = int(request.headers.get('hopcount', 0))
+    
+    # Increment hop count
+    hopcount += 1
+    
+    if hopcount > MAX_HOPS:
+        return f"ServiceB: Max hop count reached with trace ID: {trace_id}"
+    
+    headers = {
+        'x-b3-traceid': trace_id,
+        'x-b3-spanid': span_id,
+        'x-b3-parentspanid': parent_span_id,
+        'x-b3-sampled': sampled,
+        'x-b3-flags': flags,
+        'hopcount': str(hopcount)
+    }
     
     # Log the outgoing request
-    logging.info(f"ServiceB: Calling ServiceA with trace ID: {trace_id}")
+    logging.info(f"ServiceB: Calling ServiceA with trace ID: {trace_id} and hopcount: {hopcount}")
     
     response = requests.get('http://servicea:8080', headers=headers)
     
